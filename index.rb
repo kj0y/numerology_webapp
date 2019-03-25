d8dee2d0d20ce8081c2a79b9aa4b692a5755e1ea
Kandis Flick <kandisflick@gmail.com>
2019-03-25 07:43:50 -0700

    correct message to include numerology number at the beginning.

--- index.rb
+++ index.rb
@@ -26,24 +26,24 @@ def get_message(birthday_number)
     case birthday_number
         #5. Assign the return value to a variable
         when 1
-            message = "One is the leader. The number one indicates the ability to stand alone and is a strong vibration. Ruled by the Sun."
+            @message = "One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
         when 2
-            message = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+            @message = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
         when 3
-            message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+            @message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
         when 4
-            message = "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+            @message = "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
         when 5
-            message = "This is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+            @message = "This is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
         when 6
-            message = "This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+            @message = "This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
         when 7
-            message = "This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+            @message = "This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
         when 8
-            message = "This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+            @message = "This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
         when 9
-            message = "This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
-        else message = "I don't have any answers for you!"
+            @message = "This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+        else @message = "I don't have any answers for you!"
     end
 end
 
@@ -54,6 +54,12 @@ end
 get '/:birthdate' do
   birthdate = params[:birthdate]
   birthday_number = calc_birthpath_num(birthdate)
-  message = get_message(birthday_number)
-  "#{message}"
+  @message = "Your numerology number is #{birthday_number}. " + get_message(birthday_number)
+  erb :index
+end
+
+get '/newpage' do
+  # contents of new newpage
+  @message = "This is a test."
+  erb :newpage
 end
